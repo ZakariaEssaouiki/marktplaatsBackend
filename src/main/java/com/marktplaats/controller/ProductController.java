46befccc0a919d943e361f92eb899dddee2ebd7b
Zakaria <z.essaouiki@ziggo.nl>
2022-11-21 15:15:08 +0100

    Niet gebruikte import weggehaald

--- src/main/java/com/marktplaats/controller/ProductController.java
+++ src/main/java/com/marktplaats/controller/ProductController.java
@@ -9,15 +9,9 @@ import com.marktplaats.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.HtmlUtils;
-
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
