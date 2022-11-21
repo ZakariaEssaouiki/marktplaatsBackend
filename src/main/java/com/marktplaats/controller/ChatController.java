1def429fae3db792e25f4f4b15a9cd77f76231c7
Zakaria <z.essaouiki@ziggo.nl>
2022-11-21 15:17:29 +0100

    Import weggehaald

--- src/main/java/com/marktplaats/controller/ChatController.java
+++ src/main/java/com/marktplaats/controller/ChatController.java
@@ -1,12 +1,12 @@
 package com.marktplaats.controller;
 
-//import com.marktplaats.model.Bericht;
+/*import com.marktplaats.model.Bericht;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;*/
 
 /*@Controller
 public class ChatController {
